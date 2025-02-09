8f96723a54c22a150859f83758a8c1b4ac8e091f
Bob <b0b4plus1@gmail.com>
2025-02-10 12:16:16 +1300

    Changed no-button when pressed, enlarges yes button

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const noButton = document.getElementById('no-button')
+const yesButton = document.getElementById('yes-button')
 const OFFSET = 30
-noButton.addEventListener('click', ()=> alert('IS THAT A YES? 🥺🥺'));
+
+let scaleFactor = 1.5
+
+noButton.addEventListener('click', ()=> {
+  scaleFactor += 1.5; 
+  yesButton.style.transform = `scale(${scaleFactor})`;
+});
 
 document.addEventListener('mousemove', (e) => {
     const x = e.pageX
